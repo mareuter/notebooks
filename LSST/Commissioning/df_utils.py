a6b0f51eccc8bf0ceb329c38872c8417d9ac7901
Michael Reuter <mareuternh@gmail.com>
2019-09-26 21:06:54 +0000

    Change summary state report notebooks to use private_sndStamps.

--- LSST/Commissioning/df_utils.py
+++ LSST/Commissioning/df_utils.py
@@ -6,3 +6,6 @@ def convert_timestamps(df: pd.DataFrame, columns: List):
     for column in columns:
         df[column] = pd.to_datetime(df[column] * 1e9, utc=True)
     return df
+
+def time_delta(t1, t2):
+    return (t1 - t2) / 1e9
