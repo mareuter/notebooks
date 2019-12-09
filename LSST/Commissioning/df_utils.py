d471efa04d70b382dfb153dd60ee22748f7f4b96
Michael Reuter <mareuternh@gmail.com>
2019-12-09 19:13:36 +0000

    Use column addition for date conversion.

--- LSST/Commissioning/df_utils.py
+++ LSST/Commissioning/df_utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def convert_timestamps(df: pd.DataFrame, columns: List):
     for column in columns:
-        df[column] = pd.to_datetime(df[column] * 1e9, utc=True)
+        df[f"{column}_date"] = pd.to_datetime(df[column] * 1e9, utc=True)
     return df
 
 def time_delta(t1, t2):
