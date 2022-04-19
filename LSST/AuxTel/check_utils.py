17b4aa1e82778d2c9774dc0c01576151c08e0587
Michael Reuter <mareuter@lsst.org>
2022-04-19 20:44:13 +0000

    Update header checking notebooks.

--- LSST/AuxTel/check_utils.py
+++ LSST/AuxTel/check_utils.py
@@ -1,17 +1,25 @@
-__all__ = ["check_float", "check"]
+__all__ = ["check_float", "check", "check_near", "check_not_empty", "get_from_json"]
 
 import numpy as np
 
+def has_key(function):
+    def wrapper(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+        except KeyError:
+            print(f"{args[0]} does not exist!")
+    return wrapper
 
-def check(values, truth, keyword):
+@has_key
+def check(keyword, values, truth):
     value = values[keyword]
     if truth == value:
         print(f"{keyword} OK")
     else:
         print(f"Problem with {keyword}: {truth} != {value}")
 
-        
-def check_float(values, truth, keyword, precision=1e-8):
+@has_key
+def check_float(keyword, values, truth, precision=1e-8):
     value = values[keyword]
     try:
         diff = np.fabs(truth - value)
@@ -21,9 +29,8 @@ def check_float(values, truth, keyword, precision=1e-8):
             print(f"Problem with {keyword}: {truth} != {value}")
     except TypeError:
         print(f"Problem with {keyword}: {truth} != {value}")
-
         
-def check_near(values, keyword1, keyword2, precision=1.0):
+def check_near(keyword1, keyword2, values, precision=1.0):
     value1 = values[keyword1]
     value2 = values[keyword2]
     diff = np.fabs(value1 - value2)
@@ -31,3 +38,17 @@ def check_near(values, keyword1, keyword2, precision=1.0):
         print(f"{keyword1} near {keyword2}")
     else:
         print(f"{keyword1} NOT near {keyword2}: {value1}, {value2}")
+
+@has_key
+def check_not_empty(keyword, values):
+    value = values[keyword]
+    if value == "" or value is None:
+        print(f"{keyword} is empty")
+    else:
+        print(f"{keyword} OK")
+
+def get_from_json(column, info):
+    series = info["results"][0]["series"][0]
+    index = series["columns"].index(column)
+    result = series["values"][0][index]
+    return result
--- LSST/ComCam/check_utils.py
+++ LSST/ComCam/check_utils.py
@@ -1,9 +1,17 @@
-__all__ = ["check_float", "check"]
+__all__ = ["check_float", "check", "check_near", "check_not_empty", "get_from_json"]
 
 import numpy as np
 
+def has_key(function):
+    def wrapper(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+        except KeyError:
+            print(f"{args[0]} does not exist!")
+    return wrapper
 
-def check(values, truth, keyword, index=None):
+@has_key
+def check(keyword, values, truth, index=None):
     value = values[keyword]
     if index is not None:
         truth = truth.split(":")[index]
@@ -12,8 +20,8 @@ def check(values, truth, keyword, index=None):
     else:
         print(f"Problem with {keyword}: {truth} != {value}")
 
-        
-def check_float(values, truth, keyword, precision=1e-8):
+@has_key
+def check_float(keyword, values, truth, precision=1e-8):
     value = values[keyword]
     try:
         diff = np.fabs(truth - value)
@@ -23,9 +31,8 @@ def check_float(values, truth, keyword, precision=1e-8):
             print(f"Problem with {keyword}: {truth} != {value}")
     except TypeError:
         print(f"Problem with {keyword}: {truth} != {value}")
-
         
-def check_near(values, keyword1, keyword2, precision=1.0):
+def check_near(keyword1, keyword2, values, precision=1.0):
     value1 = values[keyword1]
     value2 = values[keyword2]
     diff = np.fabs(value1 - value2)
@@ -33,3 +40,17 @@ def check_near(values, keyword1, keyword2, precision=1.0):
         print(f"{keyword1} near {keyword2}")
     else:
         print(f"{keyword1} NOT near {keyword2}: {value1}, {value2}")
+
+@has_key
+def check_not_empty(keyword, values):
+    value = values[keyword]
+    if value == "" or value is None:
+        print(f"{keyword} is empty")
+    else:
+        print(f"{keyword} OK")
+
+def get_from_json(column, info):
+    series = info["results"][0]["series"][0]
+    index = series["columns"].index(column)
+    result = series["values"][0][index]
+    return result
